ee92a1eb809cef4d6fedc5e785a71249834b3978
developer@lunarhook.com <801105Fxl@>
2021-02-22 21:23:25 +0800

    增加解密

--- v3_kitapp/src/pages/kit/litekitPage.js
+++ v3_kitapp/src/pages/kit/litekitPage.js
@@ -5,7 +5,8 @@ import { View, Text, Image, Button, ScrollView } from '@tarojs/components'
 import { AtAccordion, AtGrid, AtTabBar, AtIcon } from 'taro-ui'
 import './litekitPage.scss'
 import '../../theme.scss'
-
+import plumber  from '../plumbertrace'
+import WXBizDataCrypt from '../WXBizDataCrypt'
 var data1 =
   [
     {
@@ -68,11 +69,28 @@ var data2 =
       value: '数字八星'
     },
   ];
-
+let litekitPagethis=null
 export default class litekitPage extends Component {
   componentDidMount() { 
+    wx.getSetting({
+      success (res){
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              litekitPagethis.setState({login:true})
+              plumbertrace.uid_uid = getweixinunionid(res.userInfo)
+            }
+          })
+        }
+      }
+    })
         this.setState({current:1})
 
+  }
+  componentDidUpdate()
+  {
+    
   }
   constructor (props) {
     super(props)
@@ -81,7 +99,9 @@ export default class litekitPage extends Component {
     open[2]=true
     this.state = {
       open: open,
+      login:false,
     }
+    litekitPagethis = this
   }
 
     handleClick (value) {
@@ -131,44 +151,81 @@ export default class litekitPage extends Component {
     }
  
     
+    getweixinunionid(userInfo)
+    {
+      const appId='wx4c02534bf271e2f6'
+      var pc = new WXBizDataCrypt(appId, sessionKey)
+
+      var data = pc.decryptData(userInfo.encryptedData , userInfo.iv)
+    }
 
 
-
-
-
+    userInfoHandler()
+    {
+      wx.getSetting({
+        success (res){
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            wx.getUserInfo(
+             { 
+               withCredentials:true,
+              success: function(res) {
+                //plumber
+                litekitPagethis.setState({login:true})
+              }
+            })
+          }
+        }
+      })
+    }
+    showlogin(login)
+    {
+      return (<View className={'contain'}>    
+      <button  open-type="getUserInfo" onGetUserInfo={this.userInfoHandler}>获取用户信息(授权登录)</button>
+      </View>)
+    }
   render() {
-    return (
-      <View className={'contain'}>
-        <ScrollView>
-          <AtAccordion
 
-            onClick={this.handleAccordionClick.bind(this, 1)}
-            title='心理测试'
-          >
-            <AtGrid mode='square' data={data1}
-              onClick={this.GridHander.bind(this)}
-            />
-          </AtAccordion>
-          <AtAccordion
-            onClick={this.handleAccordionClick.bind(this, 2)}
-            title='周易测试'
-          >
-            <AtGrid mode='square' data={data2}
-              onClick={this.GridHander.bind(this)}
-            />
-          </AtAccordion>
-        </ScrollView>
-        <AtTabBar
-          fixed
-          tabList={[
-            { title: '开屏语', iconType: 'lightning-bolt' },
-            { title: '鹿鸣测评', iconType: 'list' },
-            { title: '我的', iconType: 'tag' }
-          ]}
-          onClick={this.handleClick.bind(this)}
-          current={1}
-        />
-      </View>
-    )
+  const { login } = this.state;
+    if(false==login)
+    {return this.showlogin(login)}
+    else{
+      return (
+        <View className={'contain'}>
+           
+          <ScrollView>
+            <AtAccordion
+  
+              onClick={this.handleAccordionClick.bind(this, 1)}
+              title='心理测试'
+            >
+              <AtGrid mode='square' data={data1}
+                onClick={this.GridHander.bind(this)}
+              />
+            </AtAccordion>
+            <AtAccordion
+              onClick={this.handleAccordionClick.bind(this, 2)}
+              title='周易测试'
+            >
+              <AtGrid mode='square' data={data2}
+                onClick={this.GridHander.bind(this)}
+              />
+            </AtAccordion>
+          </ScrollView>
+          <AtTabBar
+            fixed
+            tabList={[
+              { title: '开屏语', iconType: 'lightning-bolt' },
+              { title: '鹿鸣测评', iconType: 'list' },
+              { title: '我的', iconType: 'tag' }
+            ]}
+            onClick={this.handleClick.bind(this)}
+            current={1}
+          />
+        </View>
+      )
+    }
+    
   }
-};
+}
+
