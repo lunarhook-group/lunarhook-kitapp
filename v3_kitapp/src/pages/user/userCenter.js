8a7531b08aca91beba5eb98775ab87c6a55be96f
fanxiaolong <developer@lunarhook.com>
2023-01-09 13:54:40 +0800

    Update userCenter.js

--- v3_kitapp/src/pages/user/userCenter.js
+++ v3_kitapp/src/pages/user/userCenter.js
@@ -5,6 +5,7 @@ import { View ,Image,Text,WebView, ScrollView} from '@tarojs/components';
 import './userCenter.scss';
 import serviceimg from './assets/service.jpg'
 import study from './assets/study.jpg'
+import test from './assets/qr_code.png'
 
 import  plumber  from '../plumbertracewithoutpoucdbwithwxrequest_miniv3'
 
@@ -69,6 +70,13 @@ export default class UserCenter extends Component {
         </View>
         <View className="imageslogan">
           <Image style='width: 207px;height: 356px;background: #fff;' src='./assets/service.jpg' showMenuByLongpress={true} onTap={() => this.clickimg("service")}></Image>
+        </View>
+        <View className="imageslogan">
+          <Text>扫码加入技术支持QQ群）</Text>
+        </View>
+
+        <View className="imageslogan">
+          <Image style='width: 207px;height: 356px;background: #fff;' src='./assets/qr_code.png' showMenuByLongpress={true} onTap={() => this.clickimg("service")}></Image>
         </View>
         <View className="imageslogan">
           <Text>扫码加入技术支持QQ群）</Text>
