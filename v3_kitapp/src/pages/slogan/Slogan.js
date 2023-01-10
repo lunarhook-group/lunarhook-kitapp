94a2a9d7afef196c54984fd66aba1efa3b37944d
fanxiaolong <developer@lunarhook.com>
2023-01-10 11:43:05 +0800

    v3

--- v3_kitapp/src/pages/slogan/Slogan.js
+++ v3_kitapp/src/pages/slogan/Slogan.js
@@ -4,22 +4,32 @@ import { View, Text, ScrollView ,CustomWrapper,Image} from '@tarojs/components'
 import sloganshow from '../config/SloganModule'
 import './Slogan.scss'
 import time1 from './assets/1.jpg'
+import time2 from './assets/2.jpg'
+import time3 from './assets/3.jpg'
+import time4 from './assets/4.jpg'
+import time5 from './assets/5.jpg'
+import time6 from './assets/6.jpg'
+import time7 from './assets/7.jpg'
+import time8 from './assets/8.jpg'
+import time9 from './assets/9.jpg'
+import time10 from './assets/10.jpg'
+import time11 from './assets/11.jpg'
+import time12 from './assets/12.jpg'
 
-/*
 var imgtime = new Array()
-imgtime["子"] = imgtime1
-imgtime["丑"] = imgtime2
-imgtime["寅"] = imgtime3
-imgtime["卯"] = imgtime4
-imgtime["辰"] = imgtime5
-imgtime["巳"] = imgtime6
-imgtime["午"] = imgtime7
-imgtime["未"] = imgtime8
-imgtime["申"] = imgtime9
-imgtime["酉"] = imgtime10
-imgtime["戌"] = imgtime11
-imgtime["亥"] = imgtime12
-*/
+imgtime.push(time1)
+imgtime.push(time2)
+imgtime.push(time3)
+imgtime.push(time4)
+imgtime.push(time5)
+imgtime.push(time6)
+imgtime.push(time7)
+imgtime.push(time8)
+imgtime.push(time9)
+imgtime.push(time10)
+imgtime.push(time11)
+imgtime.push(time12)
+
 
 
 export default class Slogan extends Component {
@@ -80,18 +90,21 @@ export default class Slogan extends Component {
   }
   render() {
     const res = Taro.getSystemInfoSync()
-    //var selectday = new Date()
+    var selectday = new Date()
+    var h = (selectday.getHours()+1)%24
+    h = Math.floor((h/2)%12)
     //var curgztime = this.state.wanNianLiInfo.info.gzQuarter;
     //var imgindex = imgtime[curtimelucky[1]]
     //var second_height = 600
     //var second_width = 375
+
 
     var second_height= res.screenHeight
     var second_width = res.screenWidth
     return (
       <View className={"imagecontain"} animation={this.state.animationData}>
         <ScrollView >
-        <Image style="width: 100%;height: 100%;" src="./assets/1.jpg"></Image>
+        <Image style="width: 320px;height: 560px" src={imgtime[h]}></Image>
               <View className={"contain"}  >
 
               <CustomWrapper>
