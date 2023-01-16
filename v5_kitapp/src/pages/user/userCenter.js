e26705f98487b23d8afc52506ee6af60def8309b
fanxiaolong <developer@lunarhook.com>
2023-01-17 05:15:39 +0800

    V5 TEST

--- v5_kitapp/src/pages/user/userCenter.js
+++ v5_kitapp/src/pages/user/userCenter.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { View, Image, Text, WebView, ScrollView } from '@tarojs/components';
+import { View, Image, Text, Canvas, ScrollView,Button } from '@tarojs/components';
 import Taro from '@tarojs/taro'
 import { AtIcon, AtDivider, AtTabBar, AtList, AtListItem } from 'taro-ui'
 import './userCenter.scss';
 import '../kit/tools/icon_mdi.scss'
+import miniapp from './assets/miniapp.jpg'
 import study from './assets/study.jpg'
 import test from './assets/test.jpg'
 
@@ -45,9 +46,79 @@ export default class UserCenter extends Component {
       urls: [url],
     })
   }
+  saveImg(w,h,qrCodeSide,qrCenter) {
+    const query = Taro.createSelectorQuery()
+    query.select('#usercenter').fields({ node: true, size: true })
+    .exec(function(res){
+      const canvas = res[0].node
+      const ctx = canvas.getContext('2d')
+      ctx.draw();
+
+      setTimeout(() => {
+        Taro.canvasToTempFilePath({
+          x: 0,
+          y: 0,
+          width: w,
+          canvasId: 'usercenter',
+          canvas: 'usercenter',
+          fileType: 'jpg',
+          success(res) {
+            setTimeout(() => {
+              Taro.saveImageToPhotosAlbum({
+                filePath: res.tempFilePath,
+                success() {
+                  Taro.showToast('已保存到本地相册');
+                }
+              });
+            }, 300);
+          }
+        });
+      }, 300);
+    })
+    // 创建canvas对象
+    //const cxt = Canvas.getContext('2d')
+    // 绘制背景
+    //cxt.drawImage(miniapp, 0, 0, w, h);
+    // 绘制圆形
+    //cxt.arc(qrCenter.x, qrCenter.y, qrCodeSide / 2, 0, 2 * Math.PI);
+    // 设置裁剪，下面绘制二维码就会裁剪在圆形上
+    //cxt.clip();
+    // 绘制二维码
+    //cxt.drawImage(test, 264, 845, qrCodeSide, qrCodeSide);
+    //cxt.draw();
+    // 延迟执行才能绘制成功
+    setTimeout(() => {
+      Taro.canvasToTempFilePath({
+        x: 0,
+        y: 0,
+        width: w,
+        canvasId: 'usercenter',
+        canvas: 'usercenter',
+        fileType: 'jpg',
+        success(res) {
+          setTimeout(() => {
+            Taro.saveImageToPhotosAlbum({
+              filePath: res.tempFilePath,
+              success() {
+                Taro.showToast('已保存到本地相册');
+              }
+            });
+          }, 300);
+        }
+      });
+    }, 300);
+  };
   // 获取用户列表API
   render() {
-    const { userCenterList } = this.props
+    var res = Taro.getSystemInfoSync()
+    var w = res.windowWidth * 0.9
+    var h = res.windowHeight * 0.8
+    var qrCodeSide = 234;
+    // 二维码圆心位置
+    var qrCenter = {
+      x: 381,
+      y: 381
+    };
     return (
       <View className="container">
         <ScrollView>
@@ -64,24 +135,23 @@ export default class UserCenter extends Component {
           </View>
              
         </View> {*/}
-
+  <Canvas className='imagecontain' canvas-id="usercenter" id="usercenter" style={{ width: w, height: h }}>
           <View className="imageslogan">
             <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/study.jpg'>    <View style="opacity: 0;position: absolute; width: 100%; height: 100%; top: 0; transform-origin: top right;">
-
               <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
               <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
               <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
               <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
             </View>
             </Image>
-
+            <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/test.jpg' showMenuByLongpress={true} onTap={() => this.clickimg("service")}></Image>
             <View className="imageslogan">
               <Text>点二维码加入课程学习群</Text>
             </View>
           </View>
 
           <View className="imageslogan">
-            <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/test.jpg' showMenuByLongpress={true} onTap={() => this.clickimg("service")}></Image>
+            
           </View>
           <View className="imageslogan">
             <Text>扫码联系支持人员</Text>
@@ -91,8 +161,8 @@ export default class UserCenter extends Component {
           <View className="imageslogan">
             <Text>乾坤爻APP亦可通过各手机市场渠道获取</Text>
           </View>
-
-
+          </Canvas>
+          <Button size='mini' type='primary' onClick={()=>this.saveImg(w,h,qrCodeSide,qrCenter)}>制作海报</Button>
         </ScrollView>
         <AtTabBar
           fixed
