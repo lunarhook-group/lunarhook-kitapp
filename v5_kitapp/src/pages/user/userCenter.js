3d24e436de19c557b20d94a228dc749cdb173c68
fanxiaolong <developer@lunarhook.com>
2023-01-17 17:45:02 +0800

    tijiao

--- v5_kitapp/src/pages/user/userCenter.js
+++ v5_kitapp/src/pages/user/userCenter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Image, Text, Canvas, ScrollView,Button } from '@tarojs/components';
+import { View, Image, Text, Canvas, ScrollView, Button } from '@tarojs/components';
 import Taro from '@tarojs/taro'
 import { AtIcon, AtDivider, AtTabBar, AtList, AtListItem } from 'taro-ui'
 import './userCenter.scss';
@@ -46,69 +46,58 @@ export default class UserCenter extends Component {
       urls: [url],
     })
   }
-  saveImg(w,h,qrCodeSide,qrCenter) {
-    const query = Taro.createSelectorQuery()
-    query.select('#usercenter').fields({ node: true, size: true })
-    .exec(function(res){
-      const canvas = res[0].node
-      const ctx = canvas.getContext('2d')
-      ctx.draw();
-
-      setTimeout(() => {
-        Taro.canvasToTempFilePath({
-          x: 0,
-          y: 0,
-          width: w,
-          canvasId: 'usercenter',
-          canvas: 'usercenter',
-          fileType: 'jpg',
-          success(res) {
-            setTimeout(() => {
-              Taro.saveImageToPhotosAlbum({
-                filePath: res.tempFilePath,
-                success() {
-                  Taro.showToast('已保存到本地相册');
-                }
-              });
-            }, 300);
-          }
-        });
-      }, 300);
-    })
-    // 创建canvas对象
-    //const cxt = Canvas.getContext('2d')
-    // 绘制背景
-    //cxt.drawImage(miniapp, 0, 0, w, h);
-    // 绘制圆形
-    //cxt.arc(qrCenter.x, qrCenter.y, qrCodeSide / 2, 0, 2 * Math.PI);
-    // 设置裁剪，下面绘制二维码就会裁剪在圆形上
-    //cxt.clip();
-    // 绘制二维码
-    //cxt.drawImage(test, 264, 845, qrCodeSide, qrCodeSide);
-    //cxt.draw();
-    // 延迟执行才能绘制成功
+  saveImg(w, h, qrCodeSide, qrCenter) {
     setTimeout(() => {
-      Taro.canvasToTempFilePath({
-        x: 0,
-        y: 0,
-        width: w,
-        canvasId: 'usercenter',
-        canvas: 'usercenter',
-        fileType: 'jpg',
-        success(res) {
-          setTimeout(() => {
-            Taro.saveImageToPhotosAlbum({
-              filePath: res.tempFilePath,
-              success() {
-                Taro.showToast('已保存到本地相册');
-              }
-            });
-          }, 300);
-        }
-      });
-    }, 300);
-  };
-  // 获取用户列表API
+      Taro.nextTick(() => {
+        wx.createSelectorQuery().selectAll('#usercenter').node(res => {
+            const node = res[0]
+            if (!node) return
+            const ctx = res[0].node.getContext('2d')
+            Taro.getImageInfo({ src: 'https://www.lunarhook.com/static/img/study.jpg' })
+              .then((res) => {
+                ctx.drawImage(miniapp, 0, 0, 140, 140);
+                //ctx.drawImage("https://www.lunarhook.com/static/img/study.jpg", 160, 0, 140, 140);
+                //ctx.drawImage("https://www.lunarhook.com/static/img/study.jpg", 0, 190, 140, 140);
+                //ctx.drawImage("https://www.lunarhook.com/static/img/study.jpg", 160, 190, 140, 140);
+                ctx.draw()
+                setTimeout(() => {
+                  Taro.canvasToTempFilePath({
+                    x: 0,
+                    y: 0,
+                    width: w,
+                    height: h,
+                    canvasId: 'usercenter',
+                    fileType: 'jpg',
+                    success(res) {
+                      setTimeout(() => {
+                        Taro.saveImageToPhotosAlbum({
+                          filePath: res.tempFilePath,
+                          success() {
+                            Taro.showToast('已保存到本地相册');
+                          }
+                        });
+                      }, 300);
+                    },
+                    fail(res) {
+                      console.log(res)
+                    }
+                  });
+                }, 500)
+              })
+          }).exec()
+      })
+
+    }, 500)
+
+
+
+
+
+
+
+
+  }
+
   render() {
     var res = Taro.getSystemInfoSync()
     var w = res.windowWidth * 0.9
@@ -122,47 +111,50 @@ export default class UserCenter extends Component {
     return (
       <View className="container">
         <ScrollView>
-          {/*}
-        <View className="userinfo">
-          <View className="userinfo-avatar">
-            <AtAvatar circle openData={{ type: 'userAvatarUrl' }}></AtAvatar>
-          </View>
-          <View>
-            <View className="userinfo-nickname">
-              <open-data type="userNickName" lang="zh_CN"></open-data>
-            </View>
-            <View className="userinfo-tip"></View>
-          </View>
-             
-        </View> {*/}
-  <Canvas className='imagecontain' canvas-id="usercenter" id="usercenter" style={{ width: w, height: h }}>
-          <View className="imageslogan">
-            <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/study.jpg'>    <View style="opacity: 0;position: absolute; width: 100%; height: 100%; top: 0; transform-origin: top right;">
-              <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
-              <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
-              <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
-              <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
-            </View>
-            </Image>
-            <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/test.jpg' showMenuByLongpress={true} onTap={() => this.clickimg("service")}></Image>
+          <View className='imagecontain' style={{ width: w, height: h }}>
             <View className="imageslogan">
-              <Text>点二维码加入课程学习群</Text>
+              <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/study.jpg'>    <View style="opacity: 0;position: absolute; width: 100%; height: 100%; top: 0; transform-origin: top right;">
+                <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
+                <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
+                <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
+                <cell url='https://work.weixin.qq.com/gm/548967c3d68a0998980beabbc6e8be6a' />
+              </View>
+              </Image>
+              <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/test.jpg' showMenuByLongpress={true} onTap={() => this.clickimg("service")}></Image>
+
+            </View>
+
+            <View className="imagecontain">
+              <View className="imageslogan">
+                <Text>癸卯学习服务二维码</Text>
+                <Text>乾坤爻服务支持QQ群</Text>
+              </View>
+            </View>
+            <Text style="opacity: 0">blockline</Text>
+            <Text style="opacity: 0">blockline</Text>
+            <View className="imageslogan">
+              <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/miniapp.jpg'>    <View style="opacity: 0;position: absolute; width: 100%; height: 100%; top: 0; transform-origin: top right;">
+              </View>
+              </Image>
+              <Image style='width: 140px;height: 140px;background: #fff;' src='./assets/test.jpg' showMenuByLongpress={true} onTap={() => this.clickimg("service")}></Image>
+
+            </View>
+            <View className="imagecontain">
+              <View className="imageslogan">
+                <Text>分享乾坤爻小程序</Text>
+                <Text>下载乾坤爻原生应用</Text>
+              </View>
+            </View>
+            <View className="imageslogan">
+
+            </View>
+            <Text style="opacity: 0">blockline</Text>
+            <Text style="opacity: 0">blockline</Text>
+            <View className="imageslogan">
+              <Button size='default' type='primary' onClick={() => this.saveImg(w, h, qrCodeSide, qrCenter)}>制作分享海报</Button>
+              <Canvas type="2d" className='imagecontain' id="usercenter" style={{ width: w, height: h }}></Canvas>
             </View>
           </View>
-
-          <View className="imageslogan">
-            
-          </View>
-          <View className="imageslogan">
-            <Text>扫码联系支持人员</Text>
-          </View>
-          <Text style="opacity: 0">blockline</Text>
-          <Text style="opacity: 0">blockline</Text>
-          <View className="imageslogan">
-            <Text>乾坤爻APP亦可通过各手机市场渠道获取</Text>
-          </View>
-          </Canvas>
-          <Button size='mini' type='primary' onClick={()=>this.saveImg(w,h,qrCodeSide,qrCenter)}>制作海报</Button>
         </ScrollView>
         <AtTabBar
           fixed
